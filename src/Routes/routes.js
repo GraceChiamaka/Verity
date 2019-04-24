721d2b3925d140ab117b29e22e3f835d5fb0dcc6
Amriesgrace <obieri.grace@gmail.com>
2019-04-24 16:45:07 +0100

    Add AdminProfile page

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -7,6 +7,7 @@ import Login from '../Containers/View/Registration/Login';
 import Admin from '../Containers/View/Admin/Admin';
 import User from '../Containers/View/User/User';
 import Branch from '../Containers/View/Branch/Branch';
+import AdminProfile from '../Containers/View/Admin/AdminProfile';
 
 export default () => (
     <BrowserRouter>
@@ -18,6 +19,7 @@ export default () => (
             <Route path="/admin" component={Admin} />
             <Route path="/user" component={User} />
             <Route path="/branch" component={Branch} />
+            <Route path="/adminprofile" component={AdminProfile} />
         </div>
     </BrowserRouter>
 );
