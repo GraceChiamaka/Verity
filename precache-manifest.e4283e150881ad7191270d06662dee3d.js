ba1c5386c56b86f7807fe671a0176eecc59fa34d
Amriesgrace <obieri.grace@gmail.com>
2019-05-22 15:58:45 +0100

    Updates

---
+++ precache-manifest.e4283e150881ad7191270d06662dee3d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b071ec8585fb53abcfdb59d82dbe0a0",
+    "url": "/Verity/index.html"
+  },
+  {
+    "revision": "4b08566ede149bb01b4e",
+    "url": "/Verity/static/css/main.b96ab99e.chunk.css"
+  },
+  {
+    "revision": "0a365e12c1efbeee6ad3",
+    "url": "/Verity/static/js/2.e2b5144e.chunk.js"
+  },
+  {
+    "revision": "4b08566ede149bb01b4e",
+    "url": "/Verity/static/js/main.a5ee7ac3.chunk.js"
+  },
+  {
+    "revision": "49415b7709dc91d70c77",
+    "url": "/Verity/static/js/runtime~main.47883ded.js"
+  },
+  {
+    "revision": "f214eb68b07b16fe36bffc9a4025bf9c",
+    "url": "/Verity/static/media/Vector.f214eb68.png"
+  },
+  {
+    "revision": "e7f32b6065b16fb8a61b6924e2cc5d4d",
+    "url": "/Verity/static/media/bg-locate.e7f32b60.png"
+  },
+  {
+    "revision": "569bebb7460ae0acf15c416b98a6bce2",
+    "url": "/Verity/static/media/big_logo.569bebb7.png"
+  },
+  {
+    "revision": "9eba57075e4b11a0f9702a37a894e89f",
+    "url": "/Verity/static/media/branch.9eba5707.png"
+  },
+  {
+    "revision": "0732bf7c0f36ce6d1857e805b2777534",
+    "url": "/Verity/static/media/church.0732bf7c.png"
+  },
+  {
+    "revision": "4d9c2791f97b93b58a7cee79ede4f21a",
+    "url": "/Verity/static/media/church_logo.4d9c2791.png"
+  },
+  {
+    "revision": "722942da1bb9e0a5ba2b2eda1ec80694",
+    "url": "/Verity/static/media/post_1.722942da.png"
+  },
+  {
+    "revision": "fc3cc28c5728e89befc917717d33f81a",
+    "url": "/Verity/static/media/profile_pic.fc3cc28c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Verity/precache-manifest.eb9b6fb580c30ba6556ac4a0cb558c03.js"
+  "/Verity/precache-manifest.e4283e150881ad7191270d06662dee3d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Verity/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Verity/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
