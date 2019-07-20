94d509ef98d2a99f4e23df54834cab7430de3f97
grace <grace.obieri@appyclan.com>
2019-07-20 03:24:28 +0100

    Add updated file

--- src/Containers/View/User/userProfile.js
+++ src/Containers/View/User/userProfile.js
@@ -60,7 +60,7 @@ class UserProfile extends Component{
                                     <div className="card p-5 personal-data">
                                         <div className="row">
                                             <div className="col-md-9">
-                                                <h3>Personal details</h3>
+                                                <h3 className="text-capitalize">Personal details</h3>
                                             </div>
                                             <div className="col-md-3">
                                                 <button className="btn bg-transparent border-0 float-right" title="edit" onClick={this.showUserModal}><i className="fa fa-pencil"></i></button>
@@ -74,14 +74,43 @@ class UserProfile extends Component{
                                                             <h3 className='text-center c-brand f-bold f-20'>Update Personal details</h3>
                                                             <p className="text-center f-20 c-brown f-med f-14">Fill the form for to update your personal data</p>
                                                             <form className="personal-data-form" >
-                                                                <div className="form-group mt-5" >
-                                                                    <label htmlFor="">Full Name</label>
-                                                                    <input type="text" className="form-control" required />
+                                                                <div className="form-group row  mt-5" >
+                                                                    <div className="col-md-6">
+                                                                        <label htmlFor="">Full Name</label>
+                                                                        <input type="text" className="form-control" required />
+                                                                    </div>
+                                                                    <div className="col-md-6">
+                                                                        <label htmlFor="">Title</label>
+                                                                        <select className="form-control">
+                                                                            <option value=""></option>
+                                                                            <option value="mr">Mr</option>
+                                                                            <option value="miss">Miss</option>
+                                                                            <option value="mrs">Mrs</option>
+                                                                            <option value="ms">Ms</option>
+                                                                        </select>
+                                                                    </div>
                                                                 </div>
-                                                                <div className="form-group">
-                                                                    <label htmlFor="">Phone Number</label>
-                                                                    <input type="text" className="form-control" required />
+                                                                <div className="form-group row mt-4">
+                                                                    <div className="col-md-6">
+                                                                        <label htmlFor="">Occupation</label>
+                                                                        <input type="text" className="form-control" required/>
+                                                                    </div>
+                                                                    <div className="col-md-6">
+                                                                        <label htmlFor="">Address</label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </div>
                                                                 </div>
+                                                                <div className="form-group row">
+                                                                    <div className="col-md-6">
+                                                                        <label htmlFor=""></label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </div>
+                                                                    <div className="col-md-6">
+                                                                        <label htmlFor=""></label>
+                                                                        <input type="text" className="form-control"/>
+                                                                    </div>
+                                                                </div>
+                                                               
                                                                 <div className="form-group">
                                                                     <label htmlFor="">Username</label>
                                                                     <input type="text" className="form-control"/>
@@ -99,19 +128,39 @@ class UserProfile extends Component{
                                         
                                         <div className="row mt-5 justify-content-center">
                                             <div className="container">
-                                                <div className="col-md-10">
+                                                <div className="col-md-11">
                                                     <div className="row">
-                                                        <div className="col-md-3">
-                                                            <p>
-                                                                <strong>Name</strong>
-                                                            </p>
-                                                            <p><strong>Phone Number </strong></p>
-                                                            <p><strong>Username</strong></p>
+                                                        <div className="col-md-6">
+                                                            <p className="fw-bold">Name</p>
+                                                            <p className="fw-bold">Title</p>
+                                                            <p className="fw-bold">Occupation</p>
+                                                            <p className="fw-bold">Address</p>
+                                                            <p className="fw-bold">Phone Number </p>
+                                                            <p className="fw-bold">Gender</p>
+                                                            <p className="fw-bold">Age</p>
+                                                            <p className="fw-bold">State of Origin</p>
+                                                            <p className="fw-bold">Local Govt Area</p>
+                                                            <p className="fw-bold">Nationality</p>
+                                                            <p className="fw-bold">Marital Status</p>
+                                                            <p className="fw-bold">Church / denomination / Congregation / Fellowship you wish to join</p>
+                                                            <p className="fw-bold">Are you Baptised</p>
+                                                            <p className="fw-bold">Position held in the church if any</p>
                                                         </div>
-                                                        <div className="col-md-7">
+                                                        <div className="col-md-6">
                                                             <p>Chuck Rhoades</p>
+                                                            <p>Mr</p>
+                                                            <p>Attorney General </p>
+                                                            <p>Block 10, lemon street, yaba,lagos</p>
                                                             <p>0807 834 7782</p>
-                                                            <p>Attorney General of the State of New York</p>
+                                                            <p>Male</p>
+                                                            <p>35</p>
+                                                            <p>Imo State</p>
+                                                            <p>Mbaise</p>
+                                                            <p>German</p>
+                                                            <p>Single</p>
+                                                            <p>Winners Chapel</p>
+                                                            <p>No</p>
+                                                            <p>Usher</p>
                                                         </div>
                                                     </div> 
                                                 </div>
@@ -119,7 +168,42 @@ class UserProfile extends Component{
                                         </div>
                                     </div>
                                 
-                                </section> 
+                                </section>
+                                <section className="business-card">
+                                <div className="card p-5 business-data">
+                                        <div className="row">
+                                            <div className="col-md-9">
+                                                <h3 className="text-capitalize">Business details</h3>
+                                            </div>
+                                            <div className="col-md-3">
+                                                <button className="btn bg-transparent border-0 float-right" title="edit"><i className="fa fa-pencil"></i></button>
+                                            </div>
+                                        </div>
+                                        
+                                        <div className="row mt-5 justify-content-center">
+                                            <div className="container">
+                                                <div className="col-md-11">
+                                                    <div className="row">
+                                                        <div className="col-md-6">
+                                                            <p className="fw-bold">Business Name</p>
+                                                            <p className="fw-bold">Nature of Business</p>
+                                                            <p className="fw-bold">Business Address / Location</p>
+                                                            <p className="fw-bold">Phone Number </p>
+                                                            <p className="fw-bold">Special Offers</p>
+                                                        </div>
+                                                        <div className="col-md-6">
+                                                            <p>General Solicitors</p>
+                                                            <p>Law Firm</p>
+                                                            <p>Block 10, lemon street, yaba,lagos</p>
+                                                            <p>0807 834 7782</p>
+                                                            <p>40% discount on first purchase</p>
+                                                        </div>
+                                                    </div> 
+                                                </div>
+                                            </div>                                        
+                                        </div>
+                                    </div>
+                                </section>
                                 <section className="user-email-card mt-3">
                                     <div className="card p-5 email-data">
                                         <div className="row">
